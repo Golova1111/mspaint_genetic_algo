3030c080084f721b009b7709211e70a210d3a416
Vadym Holovashcheko <vadym.holovashchenko@nure.ua>
2022-01-16 09:26:03 +0100

    init cuda good results

--- cuda.py
+++ cuda.py
@@ -1,8 +1,55 @@
+import math
+import time
+
 import numpy as np
 from numba import cuda
 
 
 @cuda.jit
-def _cacl_elem_delta(curr_image, picture, answer):
-    pos = cuda.grid(3)
-    answer[0] += curr_image[pos] - picture[pos]
+def _calc_elem_delta(curr_image, picture, answer):
+    # p = cuda.grid(3)
+    # answer[0] += abs(curr_image[p] - picture[p])
+
+    # p = cuda.grid(2)
+    # for i in range(3):
+    #     answer[0] += abs(curr_image[p[0], p[1], i] - picture[p[0], p[1], i])
+    sum = 0
+
+    p, c = cuda.grid(2)
+    for x in range(curr_image.shape[0]):
+        sum += abs(picture[x, p, c] - curr_image[x, p, c])
+
+    # answer[p, c] = sum
+    answer[p + curr_image.shape[1] * c] = sum
+
+
+@cuda.reduce
+def sum_reduce(a, b):
+    return a + b
+
+
+def _calc_delta(device_pic, image):
+    # answer = np.zeros((image.shape[1], 3), dtype=np.int64)
+    answer = np.zeros((image.shape[1] * 3), dtype=np.int64)
+    d_image = cuda.to_device(image)
+    d_answer = cuda.to_device(answer)
+
+    # Set the number of threads in a block
+    TPB = 32
+    # threadsperblock = (TPB, TPB, 1)
+    threadsperblock = (TPB, 1)
+
+    # Calculate the number of thread blocks in the grid
+    # blockspergrid_x = int(math.ceil(image.shape[0] / threadsperblock[0]))
+    blockspergrid_y = int(math.ceil(image.shape[1] / threadsperblock[0]))
+    blockspergrid_z = 3
+
+    # blockspergrid = (blockspergrid_x, blockspergrid_y, blockspergrid_z)
+    blockspergrid = (blockspergrid_y, blockspergrid_z)
+
+    _calc_elem_delta[blockspergrid, threadsperblock](d_image, device_pic, d_answer)
+    # print("inner", end - start)
+
+    res = d_answer.copy_to_host()
+    return np.sum(res)
+    # return sum_reduce(d_answer)
--- time_test.py
+++ time_test.py
@@ -1,13 +1,14 @@
 import math
+import time
 
 import numpy as np
 
 from numba import cuda
 
-from cuda import _cacl_elem_delta
+from cuda import _calc_elem_delta, _calc_delta
 
 
-def main():
+def temp():
     picture = np.empty(shape=(360, 480, 3), dtype=np.int16)
     some_image = np.empty(shape=(360, 480, 3), dtype=np.int16)
     answer = np.zeros(1, dtype=np.int64)
@@ -28,11 +29,40 @@ def main():
     blockspergrid = (blockspergrid_x, blockspergrid_y, blockspergrid_z)
 
     # Now start the kernel
-    _cacl_elem_delta[blockspergrid, threadsperblock](d_some_image, d_picture, d_answer)
+    _calc_elem_delta[blockspergrid, threadsperblock](d_some_image, d_picture, d_answer)
     res = d_answer.copy_to_host()
 
     print(answer)
 
 
+def main():
+    picture = np.random.randint(low=0, high=255, size=(32 * 15, 32 * 20, 3), dtype=np.int16)
+    some_image = np.random.randint(low=0, high=255, size=(32 * 15, 32 * 20, 3), dtype=np.int16)
+    d_picture = cuda.to_device(picture)
+
+    # first "compilation" time
+    answer = _calc_delta(d_picture, some_image)
+
+    print(" =========== ")
+
+    start = time.time()
+    answer = _calc_delta(d_picture, some_image)
+    end = time.time()
+    print(answer)
+    print(end - start)
+
+    start = time.time()
+    answer = np.sum(np.abs(picture - some_image))
+    end = time.time()
+    print(answer)
+    print(end - start)
+
+
 if __name__ == '__main__':
+    # a = np.uint8(255)
+    # b = np.uint8(240)
+
+    # print(a - b)
+    # print(b - a)
+
     main()
