0ad13ad5acea6c1def501225c5dd57629069142a
Vadym Holovashcheko <vadym.holovashchenko@nure.ua>
2022-01-31 00:25:52 +0100

    angle mutation improve

--- Figures/Figure.py
+++ Figures/Figure.py
@@ -6,13 +6,18 @@ from Color import get_similar_color, get_color
 
 
 class Figure:
-    MUTATION_ROTATION_SCALE = 0.2
+    MUTATION_ROTATION_SCALE = 0.1
 
     def _color_mutate(self):
-        self.color, self.color_delta = get_similar_color(self.color)
+        if random.random() < 0.6:
+            self.color_delta += random.randint(-1, 1)
+            self.color_delta = min(self.color_delta, 3)
+            self.color_delta = max(-3, self.color_delta)
+        else:
+            self.color = get_similar_color(self.color)
+            self.color_delta = random.randint(-3, 3)
+
         self._repr_color = get_color(self.color, self.color_delta)
 
     def _angle_mutate(self):
-        delta = np.random.uniform(-self.MUTATION_ROTATION_SCALE, self.MUTATION_ROTATION_SCALE)
-        # print(f"{self.angle} ==> {self.angle + delta}")
-        self.angle = self.angle + delta
+        self.angle += np.random.normal(loc=0, scale=self.MUTATION_ROTATION_SCALE)
