72f5cc18c45c2c639ae39b8a1af4d9549296d6cc
Vadym Holovashcheko <vadym.holovashchenko@nure.ua>
2022-01-31 02:11:40 +0100

    update rectangle generation

--- tests/figure_test.py
+++ tests/figure_test.py
@@ -16,23 +16,14 @@ def test_color_similarity():
         print(c.color_dict[tuple(similar_color)])
 
 
-def test_picture():
+def test_picture_color_mutation():
     picture = np.random.randint(low=0, high=255, size=(240, 320, 3), dtype=np.int16)
     d_picture = cuda.to_device(picture)
     p = Picture(size=(240, 320), d_picture=d_picture)
 
-    # p.parts.append(
-    #     # Rectangle(p1=(200, 200), p2=(100, 100), color=c.RED),
-    #     Rectangle.gen_random(size=(240, 320))
-    # )
     p.parts.append(
         Rectangle(p1=(100, 100), p2=(200, 200), angle=0, color=c.LIGHTBLUE, max_size=(240, 320)),
-        # Triangle(p1=(50, 50), p2=(100, 100), p3=(50, 150), color=c.RED, max_size=(240, 320))
-        # Ellipse(center=(100, 100), a=10, b=50, color=c.RED, max_size=(240, 320), angle=0)
     )
-    # p.parts.append(
-    #     Rectangle(p1=(0, 0), p2=(100, 200), color=c.DARKGREEN),
-    # )
 
     for x in range(10):
         for y in range(10):
@@ -43,29 +34,26 @@ def test_picture():
     p.gen_picture()
     p.visualize(is_save=False)
 
-    # print(p.parts[0].color)
-    # print(p.parts[0].color_delta)
-    # p.parts[0]._color_mutate()
-    # print( ' ---------- ')
-    # print(p.parts[0].color)
-    # print(p.parts[0].color_delta)
-    # p.parts[0].color_delta = -3
-    # p.gen_picture()
-    # p.mutate()
 
-    # p.parts[0]._angle_mutate()
-    # p.parts[0].mutate()
-    # p.gen_picture()
-    # p.visualize()
+def test_picture_rectangle_generation():
+    picture = np.random.randint(low=0, high=255, size=(240, 320, 3), dtype=np.int16)
+    d_picture = cuda.to_device(picture)
+    p = Picture(size=(240, 320), d_picture=d_picture)
 
-    # p.parts[0].color_delta = 3
-    # p.gen_picture()
-    # p.mutate()
-    # p.visualize()
+    for x in range(10):
+        f = Rectangle.gen_random(size=(240, 320))
+        f.color = c.RED
+        f._color_mutate()
+
+        p.parts.append(f)
+        print(f)
+
+    p.gen_picture()
+    p.visualize(is_save=False)
 
 
 def main():
-    test_picture()
+    test_picture_rectangle_generation()
 
 
 if __name__ == '__main__':
